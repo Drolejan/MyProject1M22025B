4a97033e1436ef3b3f406f9d1d882591f7af126c
alex <>
2025-09-09 10:21:38 -0600

    iñaki no trabajo

--- Assets/muerte.cs
+++ Assets/muerte.cs
@@ -2,20 +2,18 @@ using UnityEngine;
 
 public class muerte : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Transform spawn,pelota;//Variable para la posición de mi spawn y mi pelota
+    Rigidbody2D pelotaRB;
     void Start()
     {
-        
+        //Busco el objeto Player y obtengo su componente rigidbody2d
+        pelotaRB = GameObject.Find("Player").GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("MUERTE");
+        pelota.position = spawn.position;//Hacemos que la pelota se vaya al spawn
+        pelotaRB.linearVelocity = Vector2.zero;//Reseteamos la velocidad a 0
     }
+
 }
